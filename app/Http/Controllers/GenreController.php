328ef05ecac490bb7c899020fedd9f87fc94f558
Tenjiroki <kryptorshow@gmail.com>
2025-07-29 13:58:26 +0300

    fixing

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -1,64 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Genre;
+use App\Models\Movie;
 
 class GenreController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $genres = Genre::all();
+        return response()->json($genres);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function show(Genre $genre)
     {
-        //
-    }
+        $movies = Movie::with('genres')
+            ->whereHas('genres', function ($query) use ($genre) {
+                $query->where('genre_id', $genre->id);
+            })
+            ->where('is_published', true)
+            ->paginate(10);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'genre' => $genre,
+            'movies' => $movies
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
 use App\Http\Controllers\Api\GenreController;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// API роути для тестового завдання
 Route::prefix('v1')->group(function () {
     Route::get('genres', [GenreController::class, 'index']);
     Route::get('genres/{genre}', [GenreController::class, 'show']);
